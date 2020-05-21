d2c7fe6f0bf833377cab665ef70015448af350b2
Emmanuel Ogunjirin <eao5xc@virginia.edu>
2020-05-21 16:41:04 -0400

    Sign in page and firebase implemented

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
-import React from 'react';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { auth } from "./Authentication/Firebase";
+import SignIn from "./Pages/SignIn";
+import UserProvider from "./Authentication/UserProvider";
 
+/**
+ * A private route that requires user authentication before displaying certian pages.
+ * @param {component} param0 the page component to be rendered
+ */
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      auth.currentUser ? <Component {...props} /> : <Redirect to="/signin" />
+    }
+  ></Route>
+);
+
+/**
+ * Main application that controls user and routes
+ */
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserProvider>
+      <AppRouter />
+    </UserProvider>
   );
 }
 
-export default App;
+/**
+ * Function to control routes to which pages.
+ * Conditionally renders pages based on routes and user information
+ */
+function AppRouter() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/signin" component={SignIn} />
+        <Route exact path="/" component={SignIn} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App; // Exports the application
